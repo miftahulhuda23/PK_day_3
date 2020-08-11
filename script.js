ad95e440b2b1d51bca3207d1e7a8887b0c43f8be
huda <hudami2301@gmail.com>
2020-08-11 16:01:02 +0700

    latiham hari ke 3

---
+++ script.js
@@ -0,0 +1,12 @@
+window.alert("Hello World")
+window.prompt("Siapa Nama Kamu")
+
+document.getElementById("element")
+
+console.log(document.getElementById("element"))
+
+document.body.innerHTML += "<h1>Hello World</h1>"
+
+document.body.innerHTML = document.body.innerHTML + "<h1>Hello World</h1>"
+
+document.getElementById("Element").innerHTML = "<h3>Hello World</h3>"
